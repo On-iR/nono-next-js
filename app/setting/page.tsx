bcd973cc63aead604e99cf6bba2c3e36df41d460
nophakhun <nophakhun@superboy.jp>
2023-11-25 23:05:22 +0900

    shadcn導入

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Setting() {
-  return <div>これは設定画面です</div>;
+  return (
+    <main>
+      <div>これは設定画面です</div>
+      <Button>Click me</Button>
+    </main>
+  );
 }
